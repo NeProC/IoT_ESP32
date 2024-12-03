d498182e00585dbd664815a7eb0efd30ec1a290f
idd1984 <idd@list.ru>
2024-12-04 00:42:26 +0300

    add sim800 test mqtt

---
+++ test_serial_sim800/test_serial_sim800.ino
@@ -0,0 +1,22 @@
+#include <HardwareSerial.h>
+
+
+HardwareSerial sim800(2);  // define a Serial for UART2
+//const int MySerialRX = 16;
+//const int MySerialTX = 17;
+
+
+void setup() {
+  Serial.begin(9600);  // Скорость обмена данными с компьютером
+  Serial.println("Start!");
+//  sim800.begin(9600);  // Скорость обмена данными с GSM модулем
+   sim800.begin(9600, SERIAL_8N1, 16, 17);
+  sim800.println("AT");
+}
+
+void loop() {
+  if (sim800.available())
+    Serial.write(sim800.read());
+  if (Serial.available())
+    sim800.write(Serial.read());
+}
